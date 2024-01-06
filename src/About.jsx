03c1c3db1cb6f3f4cd5700da49c52be426812a84
Frank kamgang <143176710+Frankam487@users.noreply.github.com>
2024-01-06 15:39:13 +0100

    Update About.jsx

--- src/About.jsx
+++ src/About.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <p>ici cest un paragraphe</p>
+            <p>ceci est un paragraphe</p>
         </div>
     );
 };
