1dbfbffba36bf6adbfa9e0858f80b21665677d85
Frank kamgang <143176710+Frankam487@users.noreply.github.com>
2024-01-18 00:13:50 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import Home from './Home';
 import About from './About';
 
 function App() {
+  
   return (
+    
     <BrowserRouter>
     <Routes>
     <Route path='/' element={<Home />}/>
     <Route path='/about' element={<About />}/>
-
     </Routes>
     </BrowserRouter>
   );
