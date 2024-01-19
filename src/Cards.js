87f8516bff1abb67caa3228c153aa5d28552d580
Frank kamgang <143176710+Frankam487@users.noreply.github.com>
2024-01-19 21:51:10 +0100

    Update Cards.js

--- src/Cards.js
+++ src/Cards.js
@@ -3,9 +3,11 @@ import React from "react";
 const Cards = ({ country }) => {
   return (
     <div className="cartes">
+    
     <img src={country.flag?.svg || country.flag?.png} alt={"drapeau " + country.name.common} />
     </div>
   );
 };
+
 
 export default Cards;
