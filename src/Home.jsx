2fee92d370bc63c727bf64a447e6141bbd520249
Frank kamgang <143176710+Frankam487@users.noreply.github.com>
2024-01-26 06:28:21 +0100

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Cards from "./Cards";
-const Home = () => {
+
+
+const Home = () => {  
   const [countries, setCountries] = useState([]);
   useEffect(() => {
     fetch("https://restcountries.com/v3/all").then((response) => {
