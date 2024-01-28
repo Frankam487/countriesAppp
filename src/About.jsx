6e3ed04b8785487ae6c393c2fa811ba323770fc7
Frank kamgang <143176710+Frankam487@users.noreply.github.com>
2024-01-28 20:32:35 +0100

    Update About.jsx

--- src/About.jsx
+++ src/About.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 const About = () => {
     return (
         <div className="paragraph"> 
-            
             <p>Premier paragrahe</p>
             
         </div>
